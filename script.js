7583fee24f391087bd83801ebe53f8df612fdbaf
Yuliia Yavorska <luba-09@ukr.net>
2021-11-16 15:38:04 +0200

    add remove function

--- script.js
+++ script.js
@@ -4,3 +4,8 @@ let newTask = document.getElementById("input_field").value;
 let paragraph = document.getElementById("addtoparagraph");
 paragraph.innerHTML += newTask + "   " + "<button onclick='removeTask()' type='submit' form='task1' value='Submit'>Done</button>" + "<br/>";
 }
+
+function removeTask () {
+	let paragraph = document.getElementById("addtoparagraph");
+	paragraph.remove();
+}
