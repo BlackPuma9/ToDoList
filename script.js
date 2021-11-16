a85003b1efd1c698cdb9bdd179a53e2fbe96fcce
Yuliia Yavorska <luba-09@ukr.net>
2021-11-16 14:28:52 +0200

    created html and js file

---
+++ script.js
@@ -0,0 +1,6 @@
+
+function submitNewTask() {
+let newTask = document.getElementById("input_field").value; 
+let paragraph = document.getElementById("addtoparagraph");
+paragraph.innerHTML += newTask + "   " + "<button onclick='removeTask()' type='submit' form='task1' value='Submit'>Done</button>" + "<br/>";
+}
