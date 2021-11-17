df2d89b8ba4c03871aede229b966738d908f42e6
Yuliia Yavorska <luba-09@ukr.net>
2021-11-17 14:34:40 +0200

    added done button and code releted to child element and TextNode

--- script.js
+++ script.js
@@ -1,11 +1,45 @@
+// Create a new list item when clicking on the "Add" button
+function submitTask() {
+  let li = document.createElement("li");
+  let inputValue = document.getElementById("myInput").value;
+  let t = document.createTextNode(inputValue);
+  li.appendChild(t);
+  if (inputValue === '') {
+    alert("You must write something!");
+  } else {
+    document.getElementById("myUL").appendChild(li);
+  }
+  document.getElementById("myInput").value = "";
 
-function submitNewTask() {
-let newTask = document.getElementById("input_field").value; 
-let paragraph = document.getElementById("addtoparagraph");
-paragraph.innerHTML += newTask + "   " + "<button onclick='removeTask()' type='submit' form='task1' value='Submit'>Done</button>" + "<br/>";
+  let buttonDone = document.createElement("button");
+  let txt = document.createTextNode("Done");
+  buttonDone.className = "close";
+  buttonDone.appendChild(txt);
+  li.appendChild(buttonDone);
+
+  for (let i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+      let div = this.parentElement;
+      div.style.display = "none";
+    }
+  }
 }
 
-function removeTask () {
-	let paragraph = document.getElementById("addtoparagraph");
-	paragraph.remove();
+// Create a "close" button and append it to each list item
+let myNodelist = document.getElementsByTagName("LI");
+for (let i = 0; i < myNodelist.length; i++) {
+  let buttonDone = document.createElement("button");
+  let txt = document.createTextNode("Done");
+  buttonDone.className = "close";
+  buttonDone.appendChild(txt);
+  myNodelist[i].appendChild(buttonDone);
+}
+
+// Click on a close button to hide the current list item
+let close = document.getElementsByClassName("close");
+for (let i = 0; i < close.length; i++) {
+  close[i].onclick = function() {
+    let div = this.parentElement;
+    div.style.display = "none";
+  }
 }
